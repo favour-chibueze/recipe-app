20fb360c14b0ed07bb237a6a906a3bec7c85d413
favour-chibueze <chibueze.favour.o@gmail.com>
2025-03-19 18:10:53 +0100

    add BaseNavigation component with tests and fix typo in layout

---
+++ app/components/BaseNavigation.test.ts
@@ -0,0 +1,21 @@
+import { it, expect, describe } from 'vitest'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
+import BaseNavigation from '~/components/BaseNavigation.vue'
+
+
+describe('BaseNavigation', () => {
+    it('should render a navigation bar', () => {
+        expect(true).toBe(true)
+    })
+
+    it ('should contain the navigation items', async () => {
+        const component = await mountSuspended(BaseNavigation)
+        expect(component.html()).toContain('Home')
+        expect(component.html()).toContain('About')
+    })
+
+    it('can mount the component', async () => {
+        const component = await mountSuspended(BaseNavigation)
+        expect(component.html()).toContain('Home')
+    })        
+})
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@
 
 <template>
     <div>
-        <BaseNavigaion />
+        <BaseNavigation />
         <slot />
     </div>
 </template>
